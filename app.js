74513540f0780b1dad8c3fbbc32a470e2f991bbb
mgb <mgb@alumni.brown.edu>
2022-12-10 09:13:59 -0800

    Verify OME signature in Admission Webhook

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('combined'));
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/omeapi.js
+++ routes/omeapi.js
@@ -3,54 +3,70 @@ var express = require('express');
 var router = express.Router();
 var nodeUrl = require('node:url');
 var nodePath = require('node:path');
+var crypto = require('crypto');
 
 
 module.exports = function(config) {
     const defaultStreamTime = 24 * 60 * 60 * 1000; //24 hours, in msec
+    router.use(express.text( {type: '*/*'}));
     router.post('/', function(req, res, next) {
         //for rtmp, take the stream key and remap it to the stream name with new_url
         debug("Post headers are %O", req.headers);
         debug("Post body is %O", req.body);
-        var apireq = req.body;
-        debug("Parsed body");
-        var response;
+        //verify the signature
+        var hmac = crypto.createHmac('sha1', config.apiKey);
+        hmac.update(req.body);
+        //this replace crap is honestly how OME does it (it replaces the characters and ditches the padding)
+        //https://github.com/AirenSoft/OvenMediaEngine/blob/master/src/projects/base/ovcrypto/base_64.cpp
+        var bodyDigest = hmac.digest('base64').replace('+', '-').replace('/', '_').replace(/=*$/, '');
+        debug('body Digest is %O', bodyDigest);
         var response = {};
-        if (apireq.request.direction == 'incoming') {
-            //incoming requests are for RTMP and WebRTC.  Validate that the last last part
-            //of the url is the stream key and replace it with the stream name 
-            var url = new URL(apireq.request.url);
-            var streamKey = nodePath.basename(url.pathname);
-            var streamName = nodePath.dirname(url.pathname);
-            var channel = config.channels.find((channel) => channel.streamKey === streamKey);
-            if (channel) {
-                response.allowed = true;
-                response.lifetime = defaultStreamTime;
+        var headerDigest = req.headers['x-ome-signature'];
+        if ( headerDigest === bodyDigest) {
+            var apireq = JSON.parse(req.body);
+            debug("Parsed body");
+            if (apireq.request.direction == 'incoming') {
+                //incoming requests are for RTMP and WebRTC.  Validate that the last last part
+                //of the url is the stream key and replace it with the stream name 
+                var url = new URL(apireq.request.url);
+                var streamKey = nodePath.basename(url.pathname);
+                var streamName = nodePath.dirname(url.pathname);
+                var channel = config.channels.find((channel) => channel.streamKey === streamKey);
+                if (channel) {
+                    response.allowed = true;
+                    response.lifetime = defaultStreamTime;
 
-                url.pathname = nodePath.join(streamName, channel.stream);
-                response.new_url = url.href;
+                    url.pathname = nodePath.join(streamName, channel.stream);
+                    response.new_url = url.href;
+                } else {
+                    response.allowed = false;
+                    response.reason = 'Stream key ' + streamKey + 'was not found in config';
+                    console.log('incoming stream rejected: ' + response.reason);
+                }
+
             } else {
-                response.allowed = false;
-                response.reason = 'Stream key ' + streamKey + 'was not found in config';
-                console.log('incoming stream rejected: ' + response.reason);
-            }
-
-        } else {
-            //for outgoing requests, verify that the url contains the stream key as a query paramter
-            var url = new URL(apireq.request.url);
-            var streamSecret = url.searchParams.get('streamSecret');
-            var response = {};
-            if (streamSecret == config.streamSecret) {
-                response.allowed = true;
+                //for outgoing requests, verify that the url contains the stream key as a query paramter
+                var url = new URL(apireq.request.url);
+                var streamSecret = url.searchParams.get('streamSecret');
+                var response = {};
+                if (streamSecret == config.streamSecret) {
+                    response.allowed = true;
                 //remove the stream key and redirect
-                url.searchParams.delete('streamSecret');
-                debug('searchParams are %O', url.searchParams);
-                response.new_url = url.href;
-                response.lifetime = defaultStreamTime;
-            } else {
-                response.allowed = false;
-                response.reason = 'Stream key for playback didn\'t match';
-                console.log('outgoing stream rejected: ' + response.reason);
+                    url.searchParams.delete('streamSecret');
+                    debug('searchParams are %O', url.searchParams);
+                    response.new_url = url.href;
+                    response.lifetime = defaultStreamTime;
+                } else {
+                    response.allowed = false;
+                    response.reason = 'Stream key for playback didn\'t match';
+                    console.log('outgoing stream rejected: ' + response.reason);
+                }
             }
+        } else {
+            //signature mismatch
+            response.allowed = false;
+            response.reason = 'X-OME-Signature mismatch';
+            console.log('API signature mismatch: ' + headerDigest + ' != ' + bodyDigest );
         }
         debug('API Response: %O', response);
         res.json(response);
